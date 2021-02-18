2424943e64d1a58646c4e5065ba922d22c163e04
Marcel Konnegen <marcel.konnegen@outlook.com>
2021-02-18 15:58:43 +0100

    Added documentation

--- src/Transactions/NeoTech.Core.Transactions/SinglePhaseVolatileTransactionManager.cs
+++ src/Transactions/NeoTech.Core.Transactions/SinglePhaseVolatileTransactionManager.cs
@@ -4,15 +4,23 @@ using Microsoft;
 
 namespace NeoTech.Core.Transactions
 {
+	/// <summary>
+	/// Acts as a transaction manager that handes single phase commit transactions.
+	/// </summary>
 	public class SinglePhaseVolatileTransactionManager : TwoPhaseVolatileTransactionManager, ISinglePhaseNotification
 	{
-		protected override void OnCommitInternal(Enlistment enlistment)
-		{
-			Requires.NotNull(enlistment, nameof(enlistment));
-
-			enlistment.Done();
-		}
-
+		/// <summary>
+		/// Reacts to the <see cref="IEnlistmentNotification.Prepare(PreparingEnlistment)"/> callback. Invokes all execute-actions and notifies the system
+		/// transaction manager that the operation is completed. In case of any <see cref="Exception"/> during the execution of the execute-actions,
+		/// will inform the underlying system transaction manager to force a rollback.
+		/// </summary>
+		/// <param name="preparingEnlistment">
+		/// The underlying <see cref="PreparingEnlistment"/> object.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="preparingEnlistment"/> is <c>null</c>.
+		/// </exception>
+		/// <seealso cref="TwoPhaseVolatileTransactionManager.AddExecuteCallback(Action)"/>
 		protected override void OnPrepareInternal(PreparingEnlistment preparingEnlistment)
 		{
 			Requires.NotNull(preparingEnlistment, nameof(preparingEnlistment));
@@ -32,6 +40,14 @@ namespace NeoTech.Core.Transactions
 			}
 		}
 
+		/// <summary>
+		/// Reacts to the <see cref="IEnlistmentNotification.Rollback(Enlistment)"/> callback. Sets the
+		/// <see cref="TwoPhaseVolatileTransactionManager.InFinalPhase"/> to true and then delegates back to the base implementation.
+		/// </summary>
+		/// <param name="enlistment">
+		/// The underlying <see cref="Enlistment"/> object.
+		/// </param>
+		/// <seealso cref="TwoPhaseVolatileTransactionManager.OnRollbackInternal(Enlistment)"/>.
 		protected override void OnRollbackInternal(Enlistment enlistment)
 		{
 			InFinalPhase = true;
@@ -39,6 +55,17 @@ namespace NeoTech.Core.Transactions
 			base.OnRollbackInternal(enlistment);
 		}
 
+		/// <summary>
+		/// Reacts to the <see cref="ISinglePhaseNotification.SinglePhaseCommit(SinglePhaseEnlistment)"/> callback. Invokes all execute-actions and
+		/// notifies the system transaction manager that the operation is committed. In case of any <see cref="Exception"/> during the execution
+		/// of the execute-actions, will inform the underlying system transaction manager that this commit has been aborted.
+		/// </summary>
+		/// <param name="singlePhaseEnlistment">
+		/// The underlying <see cref="SinglePhaseEnlistment"/> object.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="singlePhaseEnlistment"/> is <c>null</c>.
+		/// </exception>
 		protected virtual void OnSinglePhaseCommitInternal(SinglePhaseEnlistment singlePhaseEnlistment)
 		{
 			Requires.NotNull(singlePhaseEnlistment, nameof(singlePhaseEnlistment));
@@ -58,6 +85,12 @@ namespace NeoTech.Core.Transactions
 			}
 		}
 
+		/// <summary>
+		/// Dispatches to <see cref="OnSinglePhaseCommitInternal(SinglePhaseEnlistment)"/>.
+		/// </summary>
+		/// <param name="singlePhaseEnlistment">
+		/// The underlying <see cref="SinglePhaseEnlistment"/> object.
+		/// </param>
 		void ISinglePhaseNotification.SinglePhaseCommit(SinglePhaseEnlistment singlePhaseEnlistment)
 		{
 			OnSinglePhaseCommitInternal(singlePhaseEnlistment);
--- src/Transactions/NeoTech.Core.Transactions/TwoPhaseVolatileTransactionManager.cs
+++ src/Transactions/NeoTech.Core.Transactions/TwoPhaseVolatileTransactionManager.cs
@@ -5,21 +5,48 @@ using Microsoft;
 
 namespace NeoTech.Core.Transactions
 {
+	/// <summary>
+	/// Acts as a transaction manager that handles two-phase-commit transactions.
+	/// </summary>
 	public class TwoPhaseVolatileTransactionManager : ITransactionManager, IEnlistmentNotification
 	{
+		/// <summary>
+		/// Gets or sets the current enlistment, If this transaction manager is enlisted with a running transaction.
+		/// </summary>
 		protected Enlistment CurrentEnlistment { get; set; }
 
+		/// <summary>
+		/// Gets or sets an indicator if this transaction manager has entered the Commit phase.
+		/// </summary>
 		protected bool InFinalPhase { get; set; }
 
+		/// <summary>
+		/// Gets the list of actions to be invoked when the transaction work should be completed.
+		/// </summary>
 		protected IList<Action> OnCompletionActions { get; }
 			= new List<Action>();
 
+		/// <summary>
+		/// Gets the list of actions to be invoked when the transaction should be rolled back.
+		/// </summary>
 		protected IList<Action> OnRollbackActions { get; }
 			= new List<Action>();
 
+		/// <summary>
+		/// Gets the list of actions to be invoked when the transaction work should be executed.
+		/// </summary>
 		protected IList<Action> OnExecuteActions { get; }
 			= new List<Action>();
 
+		/// <summary>
+		/// Adds a callback to be invoked during the completion phase.
+		/// </summary>
+		/// <param name="callback">
+		/// The callback to be invoked.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="callback"/> is <c>null</c>.
+		/// </exception>
 		public void AddCompletionCallback(Action callback)
 		{
 			Requires.NotNull(callback, nameof(callback));
@@ -32,6 +59,15 @@ namespace NeoTech.Core.Transactions
 			OnCompletionActions.Add(callback);
 		}
 
+		/// <summary>
+		/// Adds a callback to be invoked during the rollback phase.
+		/// </summary>
+		/// <param name="callback">
+		/// The callback to be invoked.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="callback"/> is <c>null</c>.
+		/// </exception>
 		public void AddRollbackCallback(Action callback)
 		{
 			Requires.NotNull(callback, nameof(callback));
@@ -44,6 +80,15 @@ namespace NeoTech.Core.Transactions
 			OnRollbackActions.Add(callback);
 		}
 
+		/// <summary>
+		/// Adds a callback to be invoked during the execution phase.
+		/// </summary>
+		/// <param name="callback">
+		/// The callback to be invoked.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="callback"/> is <c>null</c>.
+		/// </exception>
 		public void AddExecuteCallback(Action callback)
 		{
 			Requires.NotNull(callback, nameof(callback));
@@ -56,6 +101,12 @@ namespace NeoTech.Core.Transactions
 			OnExecuteActions.Add(callback);
 		}
 
+		/// <summary>
+		/// Must be called at least once when a transaction has begun so this transaction manager can enlist hinself.
+		/// </summary>
+		/// <returns>
+		/// <c>true</c> if has, or already was, successfully enlisted in a transaction, else <c>false</c>.
+		/// </returns>
 		public bool TrySubscribe()
 		{
 			if (InFinalPhase)
@@ -72,6 +123,16 @@ namespace NeoTech.Core.Transactions
 			return CurrentEnlistment != null;
 		}
 
+		/// <summary>
+		/// Reacts to the <see cref="IEnlistmentNotification.Commit(Enlistment)"/> callback.
+		/// </summary>
+		/// <param name="enlistment">
+		/// The underlying <see cref="Enlistment"/> object.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="enlistment"/> is <c>null</c>.
+		/// </exception>
+		/// <see cref="AddCompletionCallback(Action)"/>
 		protected virtual void OnCommitInternal(Enlistment enlistment)
 		{
 			Requires.NotNull(enlistment, nameof(enlistment));
@@ -82,6 +143,15 @@ namespace NeoTech.Core.Transactions
 			enlistment.Done();
 		}
 
+		/// <summary>
+		/// Reacts to the <see cref="IEnlistmentNotification.InDoubt(Enlistment)"/> callback.
+		/// </summary>
+		/// <param name="enlistment">
+		/// The underlying <see cref="Enlistment"/> object.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="enlistment"/> is <c>null</c>.
+		/// </exception>
 		protected virtual void OnInDoubtInternal(Enlistment enlistment)
 		{
 			Requires.NotNull(enlistment, nameof(enlistment));
@@ -89,6 +159,18 @@ namespace NeoTech.Core.Transactions
 			enlistment.Done();
 		}
 
+		/// <summary>
+		/// Reacts to the <see cref="IEnlistmentNotification.Prepare(PreparingEnlistment)"/> callback. Invokes all execute-actions and notifies the system
+		/// transaction manager that the transaction manager is ready to be committed. In case of any <see cref="Exception"/> during the execution of the execute-actions,
+		/// will inform the underlying system transaction manager to force a rollback.
+		/// </summary>
+		/// <param name="preparingEnlistment">
+		/// The underlying <see cref="PreparingEnlistment"/> object.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="preparingEnlistment"/> is <c>null</c>.
+		/// </exception>
+		/// <seealso cref="AddExecuteCallback(Action)"/>
 		protected virtual void OnPrepareInternal(PreparingEnlistment preparingEnlistment)
 		{
 			Requires.NotNull(preparingEnlistment, nameof(preparingEnlistment));
@@ -108,6 +190,16 @@ namespace NeoTech.Core.Transactions
 			}
 		}
 
+		/// <summary>
+		/// Reacts to the <see cref="IEnlistmentNotification.Rollback(Enlistment)"/> callback. Executes all rollback actions.
+		/// </summary>
+		/// <param name="enlistment">
+		/// The underlying <see cref="Enlistment"/> object.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="enlistment"/> is <c>null</c>.
+		/// </exception>
+		/// <seealso cref="AddRollbackCallback(Action)"/>
 		protected virtual void OnRollbackInternal(Enlistment enlistment)
 		{
 			Requires.NotNull(enlistment, nameof(enlistment));
@@ -118,21 +210,48 @@ namespace NeoTech.Core.Transactions
 			enlistment.Done();
 		}
 
+		/// <summary>
+		/// Reacts to the <see cref="IEnlistmentNotification.Rollback(Enlistment)"/> callback.
+		/// </summary>
+		/// <param name="enlistment">
+		/// The underlying <see cref="Enlistment"/> object.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		/// If <paramref name="enlistment"/> is <c>null</c>.
+		/// </exception>
 		void IEnlistmentNotification.Commit(Enlistment enlistment)
 		{
 			OnCommitInternal(enlistment);
 		}
 
+		/// <summary>
+		/// Dispatches to the <see cref="OnInDoubtInternal(Enlistment)"/> method.
+		/// </summary>
+		/// <param name="enlistment">
+		/// The underlying <see cref="Enlistment"/>.
+		/// </param>
 		void IEnlistmentNotification.InDoubt(Enlistment enlistment)
 		{
 			OnInDoubtInternal(enlistment);
 		}
 
+		/// <summary>
+		/// Dispatches to the <see cref="OnPrepareInternal(PreparingEnlistment)"/> method.
+		/// </summary>
+		/// <param name="preparingEnlistment">
+		/// The underlying <see cref="PreparingEnlistment"/>.
+		/// </param>
 		void IEnlistmentNotification.Prepare(PreparingEnlistment preparingEnlistment)
 		{
 			OnPrepareInternal(preparingEnlistment);
 		}
 
+		/// <summary>
+		/// Dispatches to the <see cref="OnRollbackInternal(Enlistment)"/> method.
+		/// </summary>
+		/// <param name="enlistment">
+		/// The underlying <see cref="Enlistment"/>.
+		/// </param>
 		void IEnlistmentNotification.Rollback(Enlistment enlistment)
 		{
 			OnRollbackInternal(enlistment);
